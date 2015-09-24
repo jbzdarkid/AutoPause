93dcd3823691bb255cd8f614f2d0d01d2b0cdd4e
jbzdarkid <jbzdarkid@gmail.com>
2015-09-24 15:27:19 -0400

    Build scripts

---
+++ compile.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+
+if [[ $# -ne 0 ]]; then
+	for i in "$@";
+	do
+		smxfile="`echo $i | sed -e 's/\.sp$/\.smx/'`";
+		echo -e "Compiling $i...";
+		./spcomp $i -o$smxfile
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
+else
+	for sourcefile in *.sp
+	do
+		smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
+		echo -e "Compiling $sourcefile ..."
+		./spcomp $sourcefile -o$smxfile
+		RETVAL=$?
+		if [ $RETVAL -ne 0 ]; then
+			exit 1;
+		fi
+	done
+fi
